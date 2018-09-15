35b5d9668d8b7ad118a91d0798b707edfe725d71
trobbierob <troberts@my.hpu.edu>
2018-09-15 01:32:53 -0400

    display and play video

--- app/src/main/java/com/example/android/bakingapp/RecipeDetailFragment.java
+++ app/src/main/java/com/example/android/bakingapp/RecipeDetailFragment.java
@@ -1,18 +1,28 @@
 package com.example.android.bakingapp;
 
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import adapters.IngredientListAdapter;
-import adapters.StepListAdapter;
-import model.Recipe;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
 import model.Step;
 
 
@@ -22,16 +32,28 @@ import model.Step;
 public class RecipeDetailFragment extends android.support.v4.app.Fragment {
 
     private static final String TAG = "**DETAIL ACTIVITY LOG**";
-
-    public Recipe mRecipe;
     public Step mStep;
-    private IngredientListAdapter mAdapter;
-    private StepListAdapter sAdapter;
-    private RecyclerView mRecyclerView;
-    private RecyclerView sRecyclerView;
+
+    // bandwidth meter to measure and estimate bandwidth
+    private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
+
+    private SimpleExoPlayer player;
+    private PlayerView playerView;
+
+    private long playbackPosition;
+    private int currentWindow;
+    private boolean playWhenReady = true;
 
     public RecipeDetailFragment() {
         // Required empty public constructor
+    }
+
+    public static RecipeDetailFragment newInstance(Step step) {
+        Bundle args = new Bundle();
+        args.putParcelable("details_step", step);
+        RecipeDetailFragment fragment = new RecipeDetailFragment();
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
@@ -45,29 +67,95 @@ public class RecipeDetailFragment extends android.support.v4.app.Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.content_recipe_detail, container, false);
 
-
+        playerView = rootView.findViewById(R.id.playerView);
 
         Bundle bundle = getArguments();
         if (bundle != null){
 
             mStep = bundle.getParcelable("details_step");
             ((TextView) rootView.findViewById(R.id.step_description)).setText(String.valueOf(mStep.getDescription()));
+            initializePlayer();
             Log.i(TAG, "mStep is: " + mStep);
             Log.i(TAG, "mStep is: " + mStep.getDescription());
-
 
         } else {
             throw new AssertionError();
         }
-
         return rootView;
     }
 
-    public static RecipeDetailFragment newInstance(Step step) {
-        Bundle args = new Bundle();
-        args.putParcelable("details_step", step);
-        RecipeDetailFragment fragment = new RecipeDetailFragment();
-        fragment.setArguments(args);
-        return fragment;
+    private void initializePlayer() {
+        player = ExoPlayerFactory.newSimpleInstance(
+                new DefaultRenderersFactory(getActivity()),
+                new DefaultTrackSelector(), new DefaultLoadControl());
+
+        playerView.setPlayer(player);
+
+        player.setPlayWhenReady(playWhenReady);
+
+        Uri uri = Uri.parse(mStep.getVideoURL());
+        MediaSource mediaSource = buildMediaSource(uri);
+        player.prepare(mediaSource, true, false);
     }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        return new ExtractorMediaSource.Factory(
+                new DefaultHttpDataSourceFactory("Baking-App")).
+                createMediaSource(uri);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer();
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            player.release();
+            player = null;
+        }
+    }
+
+
 }
